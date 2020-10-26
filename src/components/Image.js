042c8c520ad1eee24e97880abb309e46681cec3c
Alboyz <alboyzskeaters@gmail.com>
2020-10-27 02:08:15 +0700

    component inside component

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,22 +1,30 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 function Image() {
-  //const [myInterval, setmyInterval] = useState(null);
-  
-  useEffect(() => {
-    console.log("Images comp Mounted");
-     const interval = setInterval(() => {
-        console.log("Hallo");
-      }, 1000);
-    
-    return()=>clearInterval(interval)
-  }, []);
+  //basic component inside component
 
+  const Images = [
+    "https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1311&q=80",
+    "https://images.unsplash.com/photo-1489824904134-891ab64532f1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80",
+    "https://images.unsplash.com/photo-1476419972179-ac981d01257e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
+  ];
+
+  function ShowImages() {
+    return Images.map((image) => {
+      return (
+        <div>
+          <img src={image} alt="" width="150" />
+        </div>
+      );
+    });
+  }
   return (
-    <img
-      src="https://images.unsplash.com/photo-1592419044706-39796d40f98c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1311&q=80"
-      alt="imagehalo"
-    />
+    <section>
+      <div className="flex justify-center">
+        <ShowImages/>
+      </div>
+    </section>
   );
 }
+
 export default Image;
