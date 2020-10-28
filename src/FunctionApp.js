63696bd9971088f7573c4ffb8ca57c2a64a769a0
Alboyz <alboyzskeaters@gmail.com>
2020-10-28 22:50:14 +0700

    spread operator remove

--- src/FunctionApp.js
+++ src/FunctionApp.js
@@ -1,31 +1,10 @@
 import Image from "./components/Image";
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, } from "react";
 import "./assets/css/style.css";
 
 function FuntionApp() {
   const [title] = useState("Click Image");
-  const [isShowing, setIsshowing] = useState(false);
-  const [didMount, setdidMount] = useState(false);
-  const monuntRef = useRef(false);
-
-  //component did  Mount Only
-  useEffect(() => {
-    setdidMount(true);
-    console.log("app mounted");
-  }, []);
-
-  //component will update
-  useEffect(() => {
-    if (monuntRef.current) {
-      console.log("App Update");
-    } else {
-      monuntRef.current = true;
-    }
-  }, [isShowing]);
-
-  function handleClick() {
-    setIsshowing(!isShowing);
-  }
+  
 
   return (
     <section className="flex justify-center">
@@ -33,16 +12,8 @@ function FuntionApp() {
       <div className="w-1/2">
         <div className="text-center">
           <div className="my-4">{title}</div>
-          <button
-            className="p-1 bg-blue-700 text-white my-2"
-            onClick={() => {
-              handleClick();
-            }}
-          >
-            Toggle Image
-          </button>
+          <Image />
         </div>
-        {isShowing ? <Image /> : null}
       </div>
     </section>
   );
--- src/components/Image.js
+++ src/components/Image.js
@@ -8,32 +8,46 @@ function Image() {
     "https://images.unsplash.com/photo-1476419972179-ac981d01257e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
   ]);
 
-  const [newImageURL, setnewImageURL] = useState("")
+  const [newImageURL, setnewImageURL] = useState("");
 
   function ShowImages() {
-    return images.map((image) => {
+    return images.map((image, index) => {
       return (
-        <div key={image} className="w-1/3">
-          <img src={image} alt="" width="150" />
+        <div key={index} className="w-1/3">
+          <img
+            src={image}
+            alt=""
+            width="150"
+            onClick={() => handleRemove(index)}
+          />
         </div>
       );
     });
   }
-  function handleAdd() {
-    setimages([
-      ...images,
-      newImageURL
-      
-      //"https://images.unsplash.com/photo-1453951115017-aa1460aa6973?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80"
+  function handleRemove(index) {
+    //you can remove with this way
+    //setimages(images.filter((image, i) => i !== index));
+    //OR
 
-    ])
-    setnewImageURL("")
-    console.log("handleAdd working");
+    //Notes: How Spread Operator Work
+    setimages([
+      ...images.slice(0, index),
+      ...images.slice(index + 1, images.length),
+    ]);
+  }
+  function handleAdd() {
+    if (newImageURL !== "") {
+      setimages([...images, newImageURL]);
+      setnewImageURL("");
+      console.log("image add");
+    } else {
+      console.log("image not add");
+    }
   }
 
   //handle change
   function handleChange(e) {
-    setnewImageURL(e.target.value)
+    setnewImageURL(e.target.value);
   }
   return (
     <section>
@@ -44,11 +58,17 @@ function Image() {
       <div className="flex justify-center my-5">
         <input
           type="text"
-          className="border border-gray-800 shadow rounded mr-4"
+          className="border border-gray-800 shadow rounded mr-4 w-full"
           onChange={handleChange}
           value={newImageURL}
         ></input>
-        <button className="p-2 bg-green-600 text-white" onClick={handleAdd}>
+        <button
+          disabled={newImageURL !== ""}
+          className={`p-2 text-white ${
+            newImageURL !== "" ? "bg-green-600" : "bg-green-300"
+          }`}
+          onClick={handleAdd}
+        >
           Add Image
         </button>
       </div>
